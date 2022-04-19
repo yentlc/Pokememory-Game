014ac8c413cf73f2790426a7e9f6b9b4b0d72e0e
yentlc <98667887+yentlc@users.noreply.github.com>
2022-04-19 14:22:15 -0300

    advance

--- MemoryGame.js
+++ MemoryGame.js
@@ -1,71 +1,60 @@
-//card options
-const cardArray = [
-    {
-        name: "charizard",
-        Img: "images/charizard.jpg"
-    },
-    {
-        name: "charizard",
-        Img: "images/charizard.jpg"
-    },
-    {
-        name: "dewgong",
-        Img: "images/dewgong.jpg"
-    },
-    {
-        name: "dewgong",
-        Img: "images/dewgong.jpg"
-    },
-    {
-        name: "dragonite",
-        Img: "images/dragonite.jpg"
-    },
-    {
-        name: "dragonite",
-        Image: "images/dragonite.jpg"
-    },
-    {
-        name: "gengar",
-        Image: "images/gengar.jpg"
-    },
-    {
-        name: "gengar",
-        Image: "images/gengar.jpg"
-    },
-    {
-        name: "venosaur",
-        Image: "images/venosaur.jpg"
-    },
-    {
-        name: "venosaur",
-        Image: "images/venosaur.jpg"
-    },
-    {
-        name: "chimchar",
-        Image: "images/chimchar.jpg"
-    },
-    {
-        name: "chimchar",
-        Image: "images/chimchar.jpg"
+document.addEventListener("DOMContentLoaded",()=>{
+
+    //card options : array de obj, cada uno es una carta (no incluye blank y grass)
+    const cardArr=[
+        {
+            name:"charizard",
+            img: "images/charizard.jpg"
+        },
+        {
+            name:"charizard",
+            img: "images/charizard.jpg"
+        },
+        {
+            name:"dewgong",
+            img: "images/dewgong.jpg"
+        },
+        {
+            name:"dewgong",
+            img: "images/dewgong.jpg"
+        },
+        {
+            name:"dragonite",
+            img: "images/dragonite.jpg"
+        },
+        {
+            name:"dragonite",
+            img: "images/dragonite.jpg"
+        },
+        {
+            name:"gengar",
+            img: "images/gengar.jpg"
+        },
+        {
+            name:"gengar",
+            img: "images/gengar.jpg"
+        },
+        {
+            name:"venosaur",
+            img: "images/venosaur.jpg"
+        },
+        {
+            name:"venosaur",
+            img: "images/venosaur.jpg"
+        },
+    ]
+
+    // creo variable grid y agarro de html la clase grid
+    const grid= document.querySelector(".grid");
+
+    //creo el board
+    function createBoard(){
+        for (let i=0;i<cardArr.length;i++){
+            var card=document.createElement("img");
+            card.setAttribute("src","images/grass.jpg")
+            card.setAttribute("data-id", i)
+            //card.addEventListener("click",flipcard)
+            grid.appendChild(card)
+        }
     }
-];
-
-
-
-const grid = document.querySelector(".grid"); //declara una variable constante llamada grid y la vincula con el elemento class .grid declarado en css y declarado en html como class="grid"
-console.log('grid', grid)
-//board game
-
-function createBoard() {
-    for (let i = 0; i < cardArray.length; i++) {  // con la funcion "for" genera un loop en el elemento cardArray poniendole .length ve el largo de el array 
-        var card = document.createElement("img");  // para cada tarjeta crea un elemento imagen a la cual declara card
-        card.setAttribute("src", "images/grass.jpg"); //linkea a la imagen blanca
-        card.setAttribute("data-id", i); // da una id que va de 0 a 11 a cada una de las cartas
-        //card.addEventListener("click", flipcard) // indico que si se clickea la carta se da vuelta y nombramos la funcion flipcard que mas adelante la codeamos 
-        grid.appendChild(card);
-
-    }
-
-}
-
-createBoard();
+})
